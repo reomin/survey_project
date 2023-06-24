3d7f9853b31e91efbb67127f5caf1a5026818ad4
reomin <takeuti29reo@gmail.com>
2023-06-24 16:57:29 +0900

    アンケート管理アプリの環境設定を行いました

---
+++ project/src/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <title>ホームページ</title>
+</head>
+
+<body>
+    <h1>ホームページ</h1>
+
+    <div class="test">
+        <form method="POST" action="{{ route('api.post') }}">
+            @csrf
+            <div>
+                <button type="submit">送信</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ project/src/resources/views/register.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <title>登録ページ</title>
+</head>
+
+<body>
+    <h1>登録ページ</h1>
+</body>
+
+</html>
